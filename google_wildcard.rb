4388c591ea90605fe8d7fb28138895eb50dc947d
joshwand <josh@joshwand.com>
2010-01-06 18:20:48 -0500

    google wildcard tool

---
+++ google_wildcard.rb
@@ -0,0 +1,80 @@
+require 'rubygems'
+require 'cgi'
+require 'open-uri'
+require 'hpricot'
+
+
+# given a wildcard search, will search google to find the most popular words to 
+# fill in the wildcard
+# e.g. "died in a * accident"
+# c.f http://xkcd.com/369/
+# 
+# Author: Josh Wand <josh@joshwand.com>
+# Distribute freely with attribution
+
+
+terms = {}
+query = %q("died in a * accident")
+
+%w(0 100 200 300 400 500).each do |num|
+  puts num + "..."
+
+  q = query.split(" ").map { |w| CGI.escape(w)}.join("+")
+
+  url = "http://www.google.com/search?hl=en&client=firefox-a&q=#{q}&num=100&start=#{num}"
+  # p url
+  doc = Hpricot(open(url).read)
+  
+  re = query.gsub("*", "([a-z'-.]+?)")
+  re.gsub!("\"", "");
+  regex = Regexp.new(re, Regexp::IGNORECASE)
+  doc.inner_html.scan(regex) do |result|
+    if terms.has_key?(result[0].downcase)
+      terms[result[0].downcase] += 1
+    else
+      terms[result[0].downcase] = 1
+    end 
+  end
+  sleep 1 + rand(20)
+end
+
+# p terms
+puts "done fetching terms. \nCounting..."
+
+
+results = {}
+
+terms.keys.each do |term|
+  q = CGI.escape(query.gsub("*",term))
+  url = "http://www.google.com/search?q=#{q}&num=5"
+  doc = Hpricot(open(url).read)
+  num = (doc/"[@id='resultStats']/b")[2].inner_html.gsub(",","").to_i
+  # p "#{term} #{num}"
+  puts "."
+  results[term] = num
+  # sleep 1 + rand(4) 
+end
+puts "done"
+
+
+results.sort{|a,b| a[1]<=>b[1]}.reverse.each do |elem|
+  width = 30
+
+  term = elem[0]
+  count = elem[1].to_s
+  
+  text = ""
+  
+  0.upto(width) do |i| 
+    if i <= term.size
+      text += term[i,1]
+    elsif i >= term.size and i < (width - count.size)
+        text += "\s"
+    else 
+      n = (i - width + count.size )
+      text += count[n, 1]
+    end
+  end
+  puts text
+  
+end
